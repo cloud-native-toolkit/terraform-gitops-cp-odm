66ce58178d7eec6d92f4db2a0986242079bd44a3
Dinesh C Pandey <dineshchandrapandey@in.ibm.com>
2022-04-08 20:10:01 +0530

    Added charts
    Signed-off-by: Dinesh C Pandey <dineshchandrapandey@in.ibm.com>

--- scripts/create-yaml.sh
+++ scripts/create-yaml.sh
@@ -7,6 +7,8 @@ CHART_DIR="$1"
 DEST_DIR="$2"
 
 ## Add logic here to put the yaml resource content in DEST_DIR
+mkdir -p "${DEST_DIR}/cp4ba-odm"
+mkdir -p "${DEST_DIR}/db-secret"
 cp -R "${CHART_DIR}/cp4ba-odm"/* "${DEST_DIR}/cp4ba-odm/"
 cp -R "${CHART_DIR}/db-secret"/* "${DEST_DIR}/db-secret"
 echo "**** Copied ${CHART_DIR} to   ${DEST_DIR}  ****"
