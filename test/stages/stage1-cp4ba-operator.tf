3b09ba6f5df1c12707e356717fffc1421a547d9f
Dinesh C Pandey <dineshchandrapandey@in.ibm.com>
2022-04-08 13:18:08 +0530

    Updated CP4BA Storageclass
    Signed-off-by: Dinesh C Pandey <dineshchandrapandey@in.ibm.com>

--- test/stages/stage1-cp4ba-operator.tf
+++ test/stages/stage1-cp4ba-operator.tf
@@ -1,10 +1,12 @@
 module "gitops_iaf_operator" {
    source = "github.com/cloud-native-toolkit/terraform-gitops-cp4ba-operator"
 
-   gitops_config = module.gitops.gitops_config
-   git_credentials = module.gitops.git_credentials
-   server_name = module.gitops.server_name
-   namespace = module.gitops_namespace.name
-   kubeseal_cert = module.gitops.sealed_secrets_cert
+  gitops_config = module.gitops.gitops_config
+  git_credentials = module.gitops.git_credentials
+  server_name = module.gitops.server_name
+  namespace = module.gitops_namespace.name
+  kubeseal_cert = module.gitops.sealed_secrets_cert
+  storageclass_operator= "portworx-db2-rwx-sc"
  }
+ 
  
