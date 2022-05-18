250dbfbfc69ef59b57b9455aac583c7d085c6ed8
Cloud-Native Toolkit <cloudnativetoolkit@gmail.com>
2022-05-18 15:17:44 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -71,7 +71,7 @@ variable "namespace" {
 variable "kubeseal_cert" {
   type        = string
   description = "The certificate/public key used to encrypt the sealed secrets"
-  default     = ""
+  #default     = ""
 }
 
 variable "server_name" {
