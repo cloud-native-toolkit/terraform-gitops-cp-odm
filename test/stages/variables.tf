4fe64898b196e4ab51f0ecada919372fb0423f0d
Cloud-Native Toolkit <cloudnativetoolkit@gmail.com>
2022-05-25 11:57:43 +0530

    Update variables.tf

--- test/stages/variables.tf
+++ test/stages/variables.tf
@@ -11,11 +11,6 @@ variable "cluster_password" {
   default     = ""
 }
 
-variable "ibmcloud_api_key" {
-  type        = string
-  description = "The api key for IBM Cloud access"
-}
-
 variable "server_url" {
   type   = string
 }
