b1c4cfa8efe8c71a5e60d74499bff3d2685fd356
Cloud-Native Toolkit <cloudnativetoolkit@gmail.com>
2022-06-01 09:30:32 +0530

    Update stage1-pullsecret.tf

--- test/stages/stage1-pullsecret.tf
+++ test/stages/stage1-pullsecret.tf
@@ -1,3 +1,13 @@
+module "global_pull_secret_synch" {
+   source = "./module"
+
+   gitops_config = module.gitops.gitops_config
+   git_credentials = module.gitops.git_credentials
+   server_name = module.gitops.server_name
+   namespace = module.gitops_namespace.name
+   kubeseal_cert = module.gitops.sealed_secrets_cert
+}
+
 module "gitops_pull_secret" {
   source = "github.com/cloud-native-toolkit/terraform-gitops-pull-secret.git"
 
