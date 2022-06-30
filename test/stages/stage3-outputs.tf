391e38d686738f5da37827405c14db385923170a
Cloud-Native Toolkit <103416270+BrahmSingh@users.noreply.github.com>
2022-06-30 16:25:01 +0530

    Update stage3-outputs.tf

--- test/stages/stage3-outputs.tf
+++ test/stages/stage3-outputs.tf
@@ -5,13 +5,13 @@ resource null_resource write_outputs {
 
     environment = {
       OUTPUT = jsonencode({
-        name        = module.cp-odm.name
-        branch      = module.cp-odm.branch
-        namespace   = module.cp-odm.namespace
-        server_name = module.cp-odm.server_name
-        layer       = module.cp-odm.layer
-        layer_dir   = module.cp-odm.layer == "infrastructure" ? "1-infrastructure" : (module.cp-odm.layer == "services" ? "2-services" : "3-applications")
-        type        = module.cp-odm.type
+        name        = module.cp4ba.name
+        branch      = module.cp4ba.branch
+        namespace   = module.cp4ba.namespace
+        server_name = module.cp4ba.server_name
+        layer       = module.cp4ba.layer
+        layer_dir   = module.cp4ba.layer == "infrastructure" ? "1-infrastructure" : (module.cp-odm.layer == "services" ? "2-services" : "3-applications")
+        type        = module.cp4ba.type
       })
     }
   }
