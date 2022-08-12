9bffb6a23f1261bbf42e4a0d911fdfc4fd77c27c
Cloud-Native Toolkit <103416270+BrahmSingh@users.noreply.github.com>
2022-08-12 13:21:46 +0530

    updated code

--- test/stages/variables.tf
+++ test/stages/variables.tf
@@ -179,7 +179,6 @@ variable "ibmcloud_api_key" {
 
 variable "storageclass" {
   type        = string
-  #default="cp4a-file-retain-gold-gid"
   #default="ibmc-vpc-block-metro-retain-10iops-tier"
   default="ocs-storagecluster-cephfs"
   description = "The storge class for operator pvc"
