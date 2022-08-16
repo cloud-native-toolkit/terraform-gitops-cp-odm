3f9d5db8de2b47d8717e310c4d607de0585d50a8
Cloud-Native Toolkit <103416270+BrahmSingh@users.noreply.github.com>
2022-08-16 10:19:50 +0530

    Update validate-deploy.sh

--- .github/scripts/validate-deploy.sh
+++ .github/scripts/validate-deploy.sh
@@ -85,14 +85,14 @@ fi
 
 ## Check if the icp4a-root-ca is there 
 count=0
-until kubectl get secret icp4a-root-ca -n "${NAMESPACE}" || [[ $count -eq 60 ]]; do
-  echo "Waiting for secret icp4a-root-ca in ${NAMESPACE} COUNTER $count" 
+until kubectl get secret icp4adeploy-root-ca -n "${NAMESPACE}" || [[ $count -eq 60 ]]; do
+  echo "Waiting for secret icp4adeploy-root-ca in ${NAMESPACE} COUNTER $count" 
   count=$((count + 1))
   sleep 60
 done
 
 if [[ $count -eq 60 ]]; then
-  echo "Timed out waiting for icp4a-root-ca in ${NAMESPACE}"
+  echo "Timed out waiting for icp4adeploy-root-ca in ${NAMESPACE}"
   exit 1
 fi
 
